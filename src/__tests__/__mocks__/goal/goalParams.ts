76328ac3ae49b2ea0a78c0e0d26e6259157a3150
MithileshHinge <mithhinge@gmail.com>
2021-11-29 06:46:22 +0530

    Major logic change: optional params should be either truthy or null; MongoDB converts 'undefined' to 'null' automatically but we should not depend on the library's implementation; TODO: the same for user, creator and squads; All current tests passed.

--- src/__tests__/__mocks__/goal/goalParams.ts
+++ src/__tests__/__mocks__/goal/goalParams.ts
@@ -2,6 +2,6 @@ import faker from '../faker';
 
 export default {
   title: faker.lorem.words(3),
-  description: [faker.lorem.paragraph(5).substr(0, 2000), undefined][faker.datatype.number(1)],
+  description: [faker.lorem.paragraph(5).substr(0, 2000), null][faker.datatype.number(1)],
   goalNumber: faker.datatype.number({ min: 1, precision: 1 }),
 };
