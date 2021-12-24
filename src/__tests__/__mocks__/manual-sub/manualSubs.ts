58faf40cbfc3903e84779342c3fe90b4633878fc
MithileshHinge <mithhinge@gmail.com>
2021-12-24 07:52:05 +0530

    Minor bug in mock newManualSub function, amount min limit 30 added;

--- src/__tests__/__mocks__/manual-sub/manualSubs.ts
+++ src/__tests__/__mocks__/manual-sub/manualSubs.ts
@@ -7,7 +7,7 @@ export default function newManualSub() {
     userId: id.createId(),
     creatorUserId: id.createId(),
     squadId: id.createId(),
-    amount: faker.datatype.number(1000),
+    amount: faker.datatype.number({ min: 30 }),
     contactNumber: ['+917507504857', '8328438934', '9923292390'][faker.datatype.number(2)],
     subscriptionStatus: faker.datatype.number(2),
   };
